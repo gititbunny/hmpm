3d259a5d8c4944634d475feeb1731a3a75995abc
Git It Bunny <gititbunny@gmail.com>
2026-07-09 05:18:36 +0200

    Update Outreach.jsx

---
+++ src/pages/Outreach.jsx
@@ -0,0 +1,10 @@
+function Outreach() {
+  return (
+    <section className="page-shell">
+      <h1>Outreach & Charity</h1>
+      <p>See how the ministry is serving and supporting the community.</p>
+    </section>
+  );
+}
+
+export default Outreach;
