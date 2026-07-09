e7858f0a74398cab525d115221d83a375154d2d1
Git It Bunny <gititbunny@gmail.com>
2026-07-09 05:16:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/global.css";
+import "./styles/navbar.css";
+import "./styles/footer.css";
+import "./styles/home.css";
+import "./styles/pages.css";
+import "./styles/forms.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
