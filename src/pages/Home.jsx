199e1e4acd8d089f029cccf13dc51f6c1b750ab2
Git It Bunny <gititbunny@gmail.com>
2026-07-09 05:17:37 +0200

    Update Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <section className="page-shell">
+      <h1>House Of Miracles Prophetic Ministries</h1>
+      <p>Welcome to the official website.</p>
+    </section>
+  );
+}
+
+export default Home;
