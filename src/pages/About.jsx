f9a43ebb3495823a7f0dd3c42a558d3e2bad1ab9
Git It Bunny <gititbunny@gmail.com>
2026-07-09 05:17:07 +0200

    Update About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <section className="page-shell">
+      <h1>About Us</h1>
+      <p>Learn more about House Of Miracles Prophetic Ministries.</p>
+    </section>
+  );
+}
+
+export default About;
