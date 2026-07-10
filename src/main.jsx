d872b14593616c2a3bd441560398b56da351e490
Git It Bunny <gititbunny@gmail.com>
2026-07-10 02:23:13 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,19 @@ import App from "./App.jsx";
 import "./styles/global.css";
 import "./styles/navbar.css";
 import "./styles/footer.css";
-import "./styles/home.css";
 import "./styles/pages.css";
 import "./styles/forms.css";
+import "./styles/home.css";
+import "./styles/about.css";
+import "./styles/ministries.css";
+import "./styles/booking.css";
+import "./styles/sermons.css";
+import "./styles/sermon-detail.css";
+import "./styles/media.css";
+import "./styles/outreach.css";
+import "./styles/contact.css";
+import "./styles/success.css";
+import "./styles/not-found.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
