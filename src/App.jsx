77f582764742658d67eb2f4a29b812248adbbbfb
Git It Bunny <gititbunny@gmail.com>
2026-08-03 01:47:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Sermons from "./pages/Sermons";
 import SermonDetail from "./pages/SermonDetail";
-import Media from "./pages/Media";
+import Testimonies from "./pages/Testimonies";
 import Ministries from "./pages/Ministries";
 import Booking from "./pages/Booking";
 import Outreach from "./pages/Outreach";
@@ -27,7 +26,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/sermons" element={<Sermons />} />
           <Route path="/sermons/:slug" element={<SermonDetail />} />
-          <Route path="/media" element={<Media />} />
+          <Route path="/testimonies" element={<Testimonies />} />
+          <Route path="/media" element={<Testimonies />} />
           <Route path="/ministries" element={<Ministries />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/outreach" element={<Outreach />} />
