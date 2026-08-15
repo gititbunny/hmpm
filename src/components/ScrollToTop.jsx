d046b9f0a5d14ebe157abf2353c11f7d2f05be0c
Git It Bunny <gititbunny@gmail.com>
2026-08-15 13:17:40 +0200

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,26 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash);
+
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }, 100);
+      }
+
+      return;
+    }
+
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, hash]);
 
   return null;
 }
